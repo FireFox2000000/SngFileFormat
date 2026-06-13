04636728d44bbeeefa0ae6df9aa4a247cbb3a4c6
Alexander "FireFox" Ong <FireFox2000000@gmail.com>
2026-06-13 12:00:49 +1000

    Vector.CopyTo span compatibility not available in net standard 2.1, copied and back-ported the implementation from MS source reference.

--- SngTool/SngLib/SngSerializer.cs
+++ SngTool/SngLib/SngSerializer.cs
@@ -79,7 +79,16 @@ namespace SngLib
                 Vector<byte> dataVector = new Vector<byte>(data.Slice(byteIndex));
 
                 Vector<byte> maskedData = dataVector ^ dataIndexVectors![lookupIndex & lookupSizeMask];
+#if NET5_0_OR_GREATER
                 maskedData.CopyTo(data.Slice(byteIndex));
+#else
+                var destination = data.Slice(byteIndex);
+                if (destination.Length < Vector<byte>.Count)
+                {
+                    throw new ArgumentException("Destination is too short");
+                }
+                Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(data.Slice(byteIndex)), maskedData);
+#endif
             }
 
             long endOfVecFilePos = filePos + totalVecElements;
